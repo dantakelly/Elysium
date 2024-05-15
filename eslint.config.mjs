a60d4bff7cc8582f5dd5d75f738fba2a40155f39
Your Namedantakelly <you@example.com>
2024-05-15 18:32:31 -0500

    Seventh Commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,12 @@
-import globals from "globals.js";
+import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-export default {
-  configs: [
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    pluginReactConfig,
-  ],
-};
+
+export default [
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+];
