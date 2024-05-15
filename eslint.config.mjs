081223d6e0c48ff19ee3b99b7b82df981be4a38f
Your Namedantakelly <you@example.com>
2024-05-15 17:46:14 -0500

    fourth commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,21 @@
-import globals from "globals";
+import globals from "globals.js";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
+const config = {
+  "extends": "next",
+  "rules": {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off"
+  }
+};
 
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReactConfig,
-];
+export default {
+  configs: [
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReactConfig,
+  ],
+};
